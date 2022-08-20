c9b5bde9d9b19f89a09fdbbbc477f7fd6ed43962
swechha <swechha@phenomic.ai>
2022-08-19 23:39:07 -0400

    add linkedlist cycle

---
+++ Fast and slow pointers/linkedlist_cycle.py
@@ -0,0 +1,35 @@
+from multiprocessing.sharedctypes import Value
+
+
+class Node:
+    def __init__(self,value) -> None:
+        self.value = Value
+        self.next = None
+
+def has_cycle(head):
+    slow, fast = head, head
+    while fast is not None and fast.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
+
+
+def main():
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
+    print("LinkedList has cycle: " + str(has_cycle(head)))
+
+    head.next.next.next.next.next.next = head.next.next
+    print("LinkedList has cycle: " + str(has_cycle(head)))
+
+    head.next.next.next.next.next.next = head.next.next.next
+    print("LinkedList has cycle: " + str(has_cycle(head)))
+
+
+main()
