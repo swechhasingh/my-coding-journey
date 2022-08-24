e68e27db301df6460018479946c20d742f64f260
Swechha Singh <swechha16.05@gmail.com>
2022-08-23 23:35:53 -0400

    Time: 100 ms (26.79%), Space: 14 MB (41.92%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -7,13 +7,11 @@ class Solution:
             source_to_destination[path[0]] = path[1]
         destination_city = None
         for s, d in source_to_destination.items():
-            curr = d
-            while curr in source_to_destination:
-                curr = source_to_destination[curr]
-            curr_destination_city = curr
+            while d in source_to_destination:
+                d = source_to_destination[d]
             if destination_city == None:
-                destination_city = curr_destination_city
-            elif destination_city != curr_destination_city:
+                destination_city = d
+            elif destination_city != d:
                 return None
         return destination_city
             
