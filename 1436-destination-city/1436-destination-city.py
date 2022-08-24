492adc076b414d66dc290dcc52a3aec9e6085ec9
Swechha Singh <swechha16.05@gmail.com>
2022-08-23 21:29:01 -0400

    Time: 76 ms (63.79%), Space: 13.9 MB (41.92%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.py
@@ -0,0 +1,17 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        source_to_destination = dict()
+        for path in paths:
+            source_to_destination[path[0]] = path[1]
+        destination_city = None
+        for s, d in source_to_destination.items():
+            curr = d
+            while curr in source_to_destination:
+                curr = source_to_destination[curr]
+            curr_destination_city = curr
+            if destination_city == None:
+                destination_city = curr_destination_city
+            elif destination_city != curr_destination_city:
+                return None
+        return destination_city
+            
