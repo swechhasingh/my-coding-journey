326253d73c66719f14a4f130f3789f4002514659
Swechha Singh <swechha16.05@gmail.com>
2022-08-23 23:42:31 -0400

    Time: 76 ms (63.79%), Space: 13.9 MB (82.69%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -2,16 +2,25 @@ class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
         # Time complexity: O(N^2)
         # Space complexity: O(N)
-        source_to_destination = dict()
-        for path in paths:
-            source_to_destination[path[0]] = path[1]
-        destination_city = None
-        for s, d in source_to_destination.items():
-            while d in source_to_destination:
-                d = source_to_destination[d]
-            if destination_city == None:
-                destination_city = d
-            elif destination_city != d:
-                return None
-        return destination_city
+        # source_to_destination = dict()
+        # for path in paths:
+        #     source_to_destination[path[0]] = path[1]
+        # destination_city = None
+        # for s, d in source_to_destination.items():
+        #     while d in source_to_destination:
+        #         d = source_to_destination[d]
+        #     if destination_city == None:
+        #         destination_city = d
+        #     elif destination_city != d:
+        #         return None
+        # return destination_city
+        visited = set()
+        notVisited = set()
+        for a, b in paths:
+            if a in notVisited:
+                notVisited.remove(a)
+            visited.add(a)
+            if b not in visited:
+                notVisited.add(b)
+        return notVisited.pop()
             
