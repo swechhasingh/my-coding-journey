41aee62a1407d6c5a894783f1230e9519b00529f
Swechha Singh <swechha16.05@gmail.com>
2022-09-11 00:29:15 -0400

    Time: 1427 ms (8.07%), Space: 14.9 MB (93.81%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -2,36 +2,57 @@ from collections import deque
 
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        queue = deque()
-        word_set = set(wordList)
-        queue.append(beginWord)
-         
-        level = 0
-        
-        # BFS traversal of the word graph
-        while queue:
-            level += 1
-            length = len(queue)
-            for i in range(length):
-                curr = queue.popleft()
-                if curr == endWord:
-                    return level
-                neighbours = self.findNeighbours(curr)
-                for word in neighbours:
-                    if word in word_set:
-                        word_set.remove(word)
-                        queue.append(word)
+        deq = collections.deque()
+        bank = set(wordList)
+        deq.append(beginWord)
+        count = 0
+        n = len(beginWord)
+        while deq:
+            for _ in range(len(deq)):
+                cur_word = deq.popleft()
+                if cur_word == endWord:
+                    return count + 1
+                for i in range(n):
+                    for ch in [chr(ord('a') + i) for i in range(26)]:
+                        new_word = f'{cur_word[:i]}{ch}{cur_word[i + 1:]}'
+                        if new_word in bank:
+                            deq.append(new_word)
+                            bank.discard(new_word)
+            count += 1
         return 0
-    
-    def findNeighbours(self, word):
-        # time complexity: O(M^2*26)
-        neighbour_nodes = []
-        alphabet = list("abcdefghijklmnopqrstuvwxyz")
+#         if endWord not in wordList:
+#             return 0
+#         queue = deque()
+#         visited = set()
         
-        for i in range(len(word)):
-            word_chars = list(word) # takes O(M)
-            for char in alphabet:
-                word_chars[i] = char
-                neighbour_nodes.append("".join(word_chars))
-        return neighbour_nodes
+#         queue.append(beginWord)
+#         visited.add(beginWord)
+        
+#         adj_list = self.findNeighbours(wordList+[beginWord])
+#         level = 1
+#         # BFS traversal of the word graph
+#         while queue:
+#             for i in range(len(queue)):
+#                 curr = queue.popleft()
+#                 if curr == endWord:
+#                     return level
+#                 for word in adj_list[curr]:
+#                     if word not in visited:
+#                         # word_set.remove(word)
+#                         queue.append(word)
+#                         visited.add(word)
+#             level += 1
+#         return 0
+    
+#     def findNeighbours(self, wordList):
+#         # time complexity: O(M^2*26)
+#         adj_list = collections.defaultdict(set)
+#         alphabet = list("abcdefghijklmnopqrstuvwxyz")
+#         for word in wordList:
+#             for i in range(len(word)):
+#                 for char in alphabet:
+#                     new_word = word[:i]+char+word[i+1:]
+#                     if new_word in wordList:
+#                         adj_list[word].add(new_word)
+#         return adj_list
         
