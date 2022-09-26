b91ae1936f18f2024fb6b5f5e8177428080c4bcf
Swechha Singh <swechha16.05@gmail.com>
2022-09-25 22:58:06 -0400

    Time: 108 ms (5.01%), Space: 14.3 MB (55.61%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -17,7 +17,6 @@ class Solution:
                 if target > nums[mid] and target <= nums[right]: 
                     left = mid + 1
                 else:
-                    right = mid - 1
-                   
+                    right = mid - 1      
         return -1
         
