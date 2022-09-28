426ec74af51310b080b1adee8b120714b028d466
Swechha Singh <swechha16.05@gmail.com>
2022-09-28 19:45:09 -0400

    Time: 331 ms (62.73%), Space: 16.8 MB (61.85%) - LeetHub

---
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.py
@@ -0,0 +1,45 @@
+class Solution:
+    """Time complexity: O(M*N), linear traversal of all elements of the grid
+    Space complexity: O(min(M*N)), DFS recursion stack space in worst case when all the cells are 1
+    """
+    def numDistinctIslands(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        # visited matrix: boolean matrix to keep track of already visited cells
+        visited= [[False for _ in range(cols)] for _ in range(rows)]
+        
+        distinct_islands = set()
+        
+        # linear traversal of grid
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1 and not visited[i][j]:
+                    island_traverse = self.__traverseIsland(grid, i, j, visited, "O")
+                    distinct_islands.add(island_traverse)
+        print(distinct_islands)
+        return len(distinct_islands)
+                
+        
+    def __traverseIsland(self, grid, x, y, visited, island_traverse):
+        """DFS traverse of island using recursion
+        """
+        # boundary cases
+        if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]):
+            return ""
+        
+        # water cells and already visited cells
+        if grid[x][y] == 0 or visited[x][y]:
+            return ""
+        
+        # mark the current cell as visited
+        visited[x][y] = True
+        
+        
+        # recursively traverse all the neighbours of (x,y) cell
+        island_traverse += self.__traverseIsland(grid, x, y+1, visited, "R") #right
+        island_traverse += self.__traverseIsland(grid, x, y-1, visited, "L") #left
+        island_traverse += self.__traverseIsland(grid, x-1, y, visited, "U") #up
+        island_traverse += self.__traverseIsland(grid, x+1, y, visited, "D") #down
+        island_traverse += 'B'
+        return island_traverse
