af4665c2f006d068c2faa9e274f5d142efbf1b51
Swechha Singh <swechha16.05@gmail.com>
2022-09-28 18:36:36 -0400

    Time: 977 ms (7.88%), Space: 16.3 MB (81.29%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,39 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        # visited matrix: boolean matrix to keep track of already visited cells
+        visited= [[False for _ in range(cols)] for _ in range(rows)]
+        
+        island_count = 0
+        
+        # linear traversal of grid
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '1' and not visited[i][j]:
+                    island_count += 1
+                    self.__traverseIsland(grid, i, j, visited)
+                
+        return island_count
+                
+        
+    def __traverseIsland(self, grid, x, y, visited):
+        # boundary cases
+        if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]):
+            return
+        
+        # water cells and already visited cells
+        if grid[x][y] == '0' or visited[x][y]:
+            return
+        
+        # mark the current cell as visited
+        visited[x][y] = True
+        
+        # recursively traverse all the neighbours of (x,y) cell
+        self.__traverseIsland(grid, x, y+1, visited) #right
+        self.__traverseIsland(grid, x, y-1, visited) #left
+        self.__traverseIsland(grid, x-1, y, visited) #up
+        self.__traverseIsland(grid, x+1, y, visited) #down
+        return
+        
