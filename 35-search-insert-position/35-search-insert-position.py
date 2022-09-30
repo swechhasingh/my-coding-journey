873732d9f4b38e28f8f4183ed1e947715cb9eea6
Swechha Singh <swechha16.05@gmail.com>
2022-09-30 19:08:19 -0400

    Time: 54 ms (91.72%), Space: 14.7 MB (82.53%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -11,10 +11,13 @@ class Solution:
             mid = (start + end)//2
             if nums[mid] == target:
                 return mid
+            
             if mid > 0 and nums[mid-1] < target and target < nums[mid]:
                 return mid
+            
             if mid < len(nums)-1 and nums[mid+1] > target and target > nums[mid]:
                 return mid + 1
+            
             if target < nums[mid]: # discard right half and search in left half
                 end = mid - 1
             elif target > nums[mid]: # discard left half and search in right half
