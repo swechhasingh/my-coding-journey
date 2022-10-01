3b9174b98b76d9260c0140f03208b60a8ca4bca7
Swechha Singh <swechha16.05@gmail.com>
2022-09-30 20:18:36 -0400

    Time: 207 ms (11.89%), Space: 15.4 MB (49.29%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,32 +1,27 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        # when input list is empty
-        
-        right_end = self.findLimit(nums, target, True)
-        if right_end == -1:
-            return [-1,-1]
-        left_end = self.findLimit(nums, target, False)
-        if left_end == -1:
-            return [-1,-1]
-        return [left_end, right_end]
+        key_range = [-1,-1]
+        key_range[1] = self.findLimit(nums, target, True)
+        if key_range[1] != -1:
+            key_range[0] = self.findLimit(nums, target, False)
+        return key_range
     
     def findLimit(self, nums, target, upper_limit):
-        
         start = 0
         end = len(nums)-1
-        keyIndex = -1
+        # targetIndex will keep track of target's last seen index
+        targetIndex = -1
         while start <= end:
             mid = (start + end)//2
-    
             if target < nums[mid]: # discard right half and search in left half
                 end = mid-1
             elif target > nums[mid]: # discard left half and search in right half
                 start = mid + 1
             else:
-                keyIndex = mid
+                targetIndex = mid  # keep track of target's last seen index
                 if upper_limit:
-                    start = mid + 1
+                    start = mid + 1 # keep looking on right handside for RHL of the range
                 else:
-                    end = mid - 1
-        return keyIndex
+                    end = mid - 1 # keep looking on left handside for LHL of the range
+        return targetIndex
     
